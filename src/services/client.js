624a17c3d0d10cfab528df8949c0b3fd260ffd60
Monika <moncobra@comcast.net>
2023-01-24 16:16:03 -0800

    fixed env, fixed client

--- src/services/client.js
+++ src/services/client.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 export const client = createClient(
-  (process.env.REACT_APP_SUPABASE_URL = 'https://jwkithqctyeqxnztsqjc.supabase.co'),
-  (process.env.REACT_APP_SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzcxNzEwMSwiZXhwIjoxOTUzMjkzMTAxfQ.HeNurSJnSyAk2ZZAejJhrc4QW0Fy_5VBQuTqDvaEdwE')
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
 );
 
 export function checkError({ data, error }) {
