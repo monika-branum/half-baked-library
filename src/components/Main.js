414240a0dc0d03db7816c8c7f76cdf2f4e462181
Monika <moncobra@comcast.net>
2023-01-24 16:20:41 -0800

    ci fixed???

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Main() {
   return (
     <main className="main">
-      <h1> Alchemys Library Catalog</h1>
+      <h1>Library Catalog</h1>
       <p>Discover what worlds await you</p>
       <Link to="/books">books</Link>
     </main>
